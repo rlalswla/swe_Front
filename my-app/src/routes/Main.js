93460419bc8796e6750bd816aed0ad751c924c13
elect10 <wlgnsakswp@naver.com>
2024-05-31 05:21:58 +0900

    main skeleton

--- my-app/src/routes/Main.js
+++ my-app/src/routes/Main.js
@@ -6,6 +6,9 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import TabBar from './TabBar';
+import PostCard from "./PostCard";
+import { CiSearch } from "react-icons/ci";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Header = styled.div`
   display: flex;
@@ -35,11 +38,113 @@ const Mainpage = styled.div`
     background: #ffffff;
 `;
 
+const PostCardWrapper = styled.div`
+  display: flex;
+  justify-content: center; // 내부 아이템을 가운데 정렬
+  width: 100%; // 폭을 100%로 설정하여 부모 컨테이너의 폭에 맞춤
+  margin-bottom: 20px; // 각 카드 아래에 마진 추가
+`;
+
+const PostList = styled.div`
+  position: relative;
+  width: 393px;
+  height: 852px;
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SearchWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    width: 90%;
+    height:5%;
+    border: none;
+    background: rgba(196, 196, 196, 0.2);
+    padding: 0;
+    margin-bottom: 15px;
+    align-self: center;
+    border-radius: 10px;
+`;
+
+const SearchIcon = styled(CiSearch)`
+    margin-left: 10px;
+`;
+
+const Searchbox = styled.input`
+    flex-grow: 1;
+    border: none;
+    background: transparent;
+    padding-left: 10px;
+    font-size: 18px;
+    font-weight: 600;
+    outline: none;
+`;
+
+const SearchButtonWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    width: 100%;
+    height: 5%;
+    
+    
+`;
+
+
+const SearchButton = styled.button`
+    background: rgba(196, 196, 196, 0.2);
+    width:30%;
+    height: 5%;
+    border: none;
+    border-radius: 10px;
+    padding: 15px;
+    margin-left: 3px;
+    margin-right: 3px;
+    cursor: pointer;
+    font-size: 12px;    
+    font-weight: 600;
+    color: black;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    justify-content: space-between;
+    
+`;
+
 
 
 const Main = () => {
+    const posts = [
+        {
+          title: "SKKU Application",
+          location: "Suwon",
+          description:
+            "We are recruiting developers and designers to join our side project.",
+          roles: ["Front-end", "Back-end", "Designer"],
+          recruitingInfo: "Recruiting 1/7",
+        },
+    
+        // 추가 게시물 정보를 여기에 포함할 수 있습니다.
+      ];
+
+    const navigate = useNavigate();
+
+    const handleCardClick = () => {
+        navigate("/applicationList"); // ApplicationList 페이지로 네비게이션
+      };
+
+    
 
 
+    const [form, setForm] = useState({
+        projectname: '',
+        status: true,
+        position: 0 ,
+        stack: [],
+    });
+    
+    const [showPopup, setShowPopup] = useState(false);
 
 
     return (
@@ -48,9 +153,23 @@ const Main = () => {
                 <Title>SKKU Recruit</Title>
             </Header>
             <Title1>All Projects</Title1>
-            <div className ="inputID">
-                <input id = "searchbox" className ="inputbox" type="text" placeholder = " Search" required />
-            </div>
+            <SearchWrapper>
+                <SearchIcon />
+                <Searchbox placeholder='Search' />
+            </SearchWrapper>
+
+            <SearchButtonWrapper>
+            <SearchButton>Status<IoIosArrowDown /></SearchButton>
+            <SearchButton>Position<IoIosArrowDown /></SearchButton>
+            <SearchButton>Tech Stack<IoIosArrowDown /></SearchButton>
+            </SearchButtonWrapper>
+
+
+            {posts.map((post, index) => (
+                <PostCardWrapper key={index} onClick={handleCardClick}>
+                    <PostCard {...post} />
+                </PostCardWrapper>
+            ))}
 
 
             <h1>main^^;;</h1>
